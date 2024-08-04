d8370aa4fad68e13cead1a953ab82a271387aefd
Khaled Khazem <khaled.khazem@yahoo.com>
2024-08-04 23:17:54 +0200

    up

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jsonwebtoken");
 
 const cors = require("cors");
+
 const app = express();
 app.use(bodyParser.json());
 const port = 5000;
 
 const { Client } = require("pg");
+
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "bubble.db.elephantsql.com",
+    port: 5432,
+    user: "qvpcbtbb",
+    password: "InubxDuQZ1hQGFpKJnmto767w7eZypll",
+    database: "qvpcbtbb",
+  },
+});
 
 const database = {
   users: [
@@ -32,6 +45,22 @@ const database = {
 
 global.sign = false;
 
+const client = new Client({
+  user: "qvpcbtbb",
+  host: "bubble.db.elephantsql.com",
+  database: "qvpcbtbb",
+  password: "InubxDuQZ1hQGFpKJnmto767w7eZypll",
+  port: 5432, // Port par défaut de PostgreSQL
+});
+
+const { v4: uuidv4 } = require("uuid");
+
+client.connect().catch((err) => console.log(err));
+
+app.use(cors("*"));
+
+app.use(express.json());
+
 app.get("/signout", (req, res) => {
   //  const { sign } = req.body;
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -41,10 +70,7 @@ app.get("/signout", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
     res.json("success");
   } else {
     res.status(400).json("error login in");
@@ -69,22 +95,6 @@ app.get("/profile/:id", (req, res) => {
   }
 });
 
-app.post("/image", (req, res) => {
-  const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      res.json(user.entries);
-    }
-
-    if (!found) {
-      res.status(404).json("No such user");
-    }
-  });
-});
-
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
   bcrypt.hash(password, null, null, function (err, hash) {
@@ -101,63 +111,63 @@ app.post("/register", (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
-const client = new Client({
-  user: "qvpcbtbb",
-  host: "bubble.db.elephantsql.com",
-  database: "qvpcbtbb",
-  password: "InubxDuQZ1hQGFpKJnmto767w7eZypll",
-  port: 5432, // Port par défaut de PostgreSQL
+app.post("/connexion", async (req, res) => {
+  const { email, motdepasse } = req.body;
+
+  const result = await knex("compte").where({
+    email,
+    motdepasse,
+  });
+
+  const user = result[0];
+
+  if (!user) return res.status(401).send("L'utilisateur n'existe pas.");
+
+  delete user.motdepasse;
+
+  const token = jwt.sign({ data: user }, "motdepassedesabrina");
+
+  return res.send({ user, token: token });
 });
 
-const { v4: uuidv4 } = require("uuid");
+// GARDIEN
+app.use((req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).send("Vous devez vous connecter");
 
-client.connect();
+  const valid = jwt.verify(token, "motdepassedesabrina");
+  if (!valid) return res.status(401).send("Vous devez vous connecter");
 
-app.use(cors());
+  req.user = valid.data;
 
-app.use(express.json());
+  next();
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      res.json(user.entries);
+    }
+
+    if (!found) {
+      res.status(404).json("No such user");
+    }
+  });
+});
 
 //  pour récupérer la liste des tâches
-app.get("/api/tasks/:id_compte", async (req, res) => {
-  if (global.sign) {
-    const id_compte = req.params.id_compte;
-    console.log(id_compte);
-    try {
-      const result = await client.query(
-        "SELECT * FROM taches WHERE id_compte = $1 ORDER BY titre",
-        [id_compte]
-      );
-      global.sign = true;
-      res.json(result.rows);
-      console.log(result.rows);
-      console.log(global.sign);
-    } catch (error) {
-      console.error("Error fetching tasks:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
-});
+app.get("/api/tasks", async (req, res) => {
+  const id_compte = req.user.id_compte;
 
-app.post("/connexion", async (req, res) => {
   try {
-    const { email, motdepasse } = req.body;
-
-    const result = await client.query(
-      "SELECT * FROM compte WHERE email = $1 AND motdepasse = $2 ",
-      [email, motdepasse]
-    );
-
-    const firstRow = result.rows[0];
-    const email_result = firstRow.email;
-    const id_compte = firstRow.id_compte;
-    //   console.log(firstRow.id_compte);
-    //  const id_compte = firstRow.id_compte;
-
-    const m_pass_result = firstRow.motdepasse;
-    if (email_result == email && m_pass_result == motdepasse) {
-      res.status(200).json({ message: "Connexion full", id_compte });
-    }
-    global.sign = true;
+    const result = await client.query("SELECT * FROM taches WHERE id_compte = $1 ORDER BY titre", [id_compte]);
+    res.json(result.rows);
+    console.log(result.rows);
+    console.log(global.sign);
   } catch (error) {
     console.error("Error fetching tasks:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -171,8 +181,7 @@ app.post("/submit-form", (req, res) => {
 
   const { nom, prenom, email, password, dateDeNaissance } = req.body; // Remplacez champ1, champ2, champ3 par les noms de vos champs de formulaire
 
-  const sqlQuery =
-    "INSERT INTO compte (id_compte, nom, prenom, motdepasse, datedenaissance, email) VALUES ($1, $2, $3, $4, $5, $6)";
+  const sqlQuery = "INSERT INTO compte (id_compte, nom, prenom, motdepasse, datedenaissance, email) VALUES ($1, $2, $3, $4, $5, $6)";
   const values = [idCompte, nom, prenom, password, dateDeNaissance, email];
 
   client.query(sqlQuery, values, (err, result) => {
@@ -187,29 +196,12 @@ app.post("/submit-form", (req, res) => {
 
 //add task
 app.post("/submit-tache", (req, res) => {
-  const {
-    titre,
-    description,
-    etat,
-    priorité,
-    date_debut,
-    date_fin,
-    id_compte,
-  } = req.body; // Remplacez champ1, champ2, champ3 par les noms de vos champs de formulaire
+  const { titre, description, etat, priorité, date_debut, date_fin, id_compte } = req.body; // Remplacez champ1, champ2, champ3 par les noms de vos champs de formulaire
 
   const id_tache = uuidv4();
   const sqlQuery =
     "INSERT INTO taches (titre, description, etat, priorité, date_debut, date_fin ,id_compte,id_tache) VALUES ($1, $2, $3, $4, $5, $6, $7,$8)";
-  const values = [
-    titre,
-    description,
-    etat,
-    priorité,
-    date_debut,
-    date_fin,
-    id_compte,
-    id_tache,
-  ];
+  const values = [titre, description, etat, priorité, date_debut, date_fin, id_compte, id_tache];
 
   client.query(sqlQuery, values, (err, result) => {
     if (err) {
@@ -224,21 +216,11 @@ app.post("/submit-tache", (req, res) => {
 
 //update task task
 app.post("/update-tache", (req, res) => {
-  const { id_tache, titre, description, etat, priorité, date_debut, date_fin } =
-    req.body; // Remplacez champ1, champ2, champ3 par les noms de vos champs de formulaire
+  const { id_tache, titre, description, etat, priorité, date_debut, date_fin } = req.body; // Remplacez champ1, champ2, champ3 par les noms de vos champs de formulaire
 
-  const sqlQuery =
-    "UPDATE taches SET titre = $2, description = $3, etat = $4, priorité = $5, date_debut = $6, date_fin = $7 WHERE id_tache = $1";
+  const sqlQuery = "UPDATE taches SET titre = $2, description = $3, etat = $4, priorité = $5, date_debut = $6, date_fin = $7 WHERE id_tache = $1";
 
-  const values = [
-    id_tache,
-    titre,
-    description,
-    etat,
-    priorité,
-    date_debut,
-    date_fin,
-  ];
+  const values = [id_tache, titre, description, etat, priorité, date_debut, date_fin];
 
   client.query(sqlQuery, values, (err, result) => {
     if (err) {
